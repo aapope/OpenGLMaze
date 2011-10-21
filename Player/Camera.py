220709930a410b5e180509cddc72abaed34a1a59
Andrew Pope <microborg@gmail.com>
2011-10-21 12:46:42 -0500

    Added camera class and render class

---
+++ Player/Camera.py
@@ -0,0 +1,48 @@
+'''Stores and changes the camera position'''
+__author__ = "Emily and Andrew"
+__date__ = "20 October 2011"
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+import math
+
+class Camera:
+    '''Stores and changes the camera position'''
+
+    def __init__(self):
+        '''Initializes everything to 0'''
+        self.pos_X = 0
+        self.pos_Y = 0
+        self.pos_Z = 0
+        self.rot_X = 0
+        self.rot_Y = 0
+        self.rot_Z = 0
+
+    def renderCamera(self):
+        '''Translates and rotates the camera to the correct position'''
+        glRotatef(-self.rot_X , 1.0, 0.0, 0.0)
+        glRotatef(-self.rot_Y , 0.0, 1.0, 0.0)
+        glRotatef(-self.rot_Z , 0.0, 0.0, 1.0)
+        glTranslatef(-self.pos_X, -self.pos_Y, -self.pos_Z)
+        
+    def rotate(self, x, y, z):
+        '''Rotates by x, y, and z'''
+        self.rot_X += x
+        self.rot_Y += y
+        self.rot_Z += z
+
+    def strafe(self, amt):
+        '''Strafes left or right, bassed on the angle'''
+        self.pos_Z += math.cos(self.rot_X*math.pi/180)*math.sin(-self.rot_Y*math.pi/180)*amt
+        self.pos_X += math.cos(self.rot_X*math.pi/180)*math.cos(self.rot_Y*math.pi/180)*amt
+        self.pos_Y += math.cos(self.rot_X*math.pi/180)*math.sin(-self.rot_Z*math.pi/180)*amt
+
+    def walk(self, amt):
+        '''Walks forward and back based on the angle you're facing'''
+        self.pos_Z += math.cos(self.rot_X*math.pi/180)*math.cos(self.rot_Y*math.pi/180)*amt
+        self.pos_X += math.cos(self.rot_X*math.pi/180)*math.sin(self.rot_Y*math.pi/180)*amt
+        self.pos_Y += math.cos(self.rot_Z*math.pi/180)*math.sin(-self.rot_X*math.pi/180)*amt
+
+    def height(self, amt):
+        '''Goes up or down. Always along the y axis'''
+        self.pos_Y += amt
